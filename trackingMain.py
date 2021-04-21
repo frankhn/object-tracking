7e046332a066f8dd0001a922d02a427a92927094
frankhn <harfrank2@gmail.com>
2021-04-21 19:51:17 +0200

    open camera with open cv meauser time

---
+++ trackingMain.py
@@ -0,0 +1,19 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+tracker = cv2.TrackerMOSSE_create()
+success, img = cap.read()
+
+bbox = cv2.selectROI("Tracking", img, False)
+tracker.init(img, bbox)
+
+while True:
+    timer = cv2.getTickCount()
+    success, img = cap.read()
+
+    fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+    cv2.putText(img, str(int(fps)), (75, 50), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 255, 255), 2)
+    cv2.imshow("Tracking", img)
+
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
